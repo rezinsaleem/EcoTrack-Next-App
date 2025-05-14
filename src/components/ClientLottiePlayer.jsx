d811fa57473165f814e408f5c683e1080aefbf77
rezinsaleem <rezinsaleem1@gmail.com>
2025-05-15 01:14:56 +0530

    lottie animation alligned

--- src/components/ClientLottiePlayer.jsx
+++ src/components/ClientLottiePlayer.jsx
@@ -3,11 +3,13 @@ import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function ClientLottiePlayer({ src }) {
   return (
-    <Player
-      autoplay
-      loop
-      src={src}
-      style={{ height: "380px", width: "380px" }}
-    />
+    <div className="w-full flex justify-center">
+      <Player
+        autoplay
+        loop
+        src={src}
+        className="w-[250px] h-[250px] sm:w-[300px] sm:h-[300px] md:w-[380px] md:h-[380px]"
+      />
+    </div>
   );
 }
